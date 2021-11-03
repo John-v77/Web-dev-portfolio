1a9cc254d754d23a9d0a5e8ce1d88981e4eb7156
John-v77 <john.vs11@hotmail.com>
2021-11-03 18:07:30 -0400

    nav bar scrolling effect

--- src/js/burgerMenu.js
+++ src/js/burgerMenu.js
@@ -8,3 +8,17 @@ burgerBtn.addEventListener('click', _ => {
 })
 
 // -----------------------------------------------
+
+
+const navBar = document.querySelector('nav')
+console.log(navBar)
+
+window.addEventListener('scroll', () =>{
+    const scrollPos = window.scrollY;
+    if(scrollPos > 10){
+        navBar.classList.add('scrolled')
+    }
+    else{
+        navBar.classList.remove('scrolled')
+    }
+})
